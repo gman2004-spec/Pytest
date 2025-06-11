c696a6de102d2ed8d4daed26addb143bc73a5d7b
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-06-11 16:53:45 -0300

    Update test_cep.py

--- test_cep.py
+++ test_cep.py
@@ -3,7 +3,7 @@ import requests
 
 @pytest.fixture
 def consulta_cep():
-    resposta = requests.get("https://viacep.com.br/ws/{cep}/json/")
+    resposta = requests.get("https://viacep.com.br/ws/01001000/json/")
     
     if resposta.status_code != 200:
         return 'Cep não encontrado'
@@ -18,5 +18,25 @@ global resposta
 def test_cep_nao_encontrado(consulta_cep):
     resposta = consulta_cep
     
-    assert not resposta == 'Cep não encontrado'
+    assert not resposta == 'Cep não encontrado' 
     
+def test_encontrar_numero_de_cep(consulta_cep):
+    resposta = consulta_cep
+    
+    #cep = resposta.json()
+    #assert cep["cep"] ==  "01001-000"
+    assert resposta.json() == {
+  "cep": "01001-000",
+  "logradouro": "Praça da Sé",
+  "complemento": "lado ímpar",
+  "unidade": "",
+  "bairro": "Sé",
+  "localidade": "São Paulo",
+  "uf": "SP",
+  "estado": "São Paulo",
+  "regiao": "Sudeste",
+  "ibge": "3550308",
+  "gia": "1004",
+  "ddd": "11",
+  "siafi": "7107"
+}   
